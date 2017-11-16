6669111eb096a928a0ba422aaf6b19d715b4a144
Brian Choe <brian@visibook.com>
2017-11-16 15:25:59 -0800

    prepping to redo ng cast to practice angular again

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,21 @@
 angular.module('video-player')
-.component('app', {
-  controller: function(youTube) {
-    this.$onInit = function () {
-      youTube.search('', this.searchResults); 
-    };
+  .component('app', {
+    controller: function(youTube) {
+      this.$onInit = function () {
+        youTube.search('', this.searchResults);
+      };
 
-    this.currentVideo = {};
-    this.videos = [];
-    youTube.search('', this.searchResults);
-    this.selectVideo = video => {
-      this.currentVideo = video;
-    };
-    this.appSearch = youTube.search;
-    this.searchResults = data => {
-      this.videos = data.items;
-      this.currentVideo = data.items[0];
-    };
-  },
-  templateUrl: 'src/templates/app.html'
-});
+      this.currentVideo = {};
+      this.videos = [];
+      youTube.search('', this.searchResults);
+      this.selectVideo = video => {
+        this.currentVideo = video;
+      };
+      this.appSearch = youTube.search;
+      this.searchResults = data => {
+        this.videos = data.items;
+        this.currentVideo = data.items[0];
+      };
+    },
+    templateUrl: 'src/templates/app.html'
+  });
